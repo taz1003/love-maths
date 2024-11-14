87b5b46f4fe0d360910ba09228b7575ddb0f9524
Tazriyan Sirat <li9vyrit@students.codeinstitute.net>
2024-11-14 13:57:53 +0000

    Add functionality to calculate correct answer for addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,8 +39,21 @@ function checkAnswer() {
 
 }
 
+/**
+ * Gets the operands (the numbers) and the opretaor (plus, minus etc.)
+ * directly from the DOM and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator' ).innerText;
 
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented opertor ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
