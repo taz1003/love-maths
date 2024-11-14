18a4d24f649941bf1b355f1605c9bdfe8f69db7d
Tazriyan Sirat <li9vyrit@students.codeinstitute.net>
2024-11-14 13:10:04 +0000

    Add code to calculate two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,11 +15,18 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
-function runGame () {
+/**
+ * The game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame() {
 
+    // Creates two random numbers betn 1 and 25
+    let num1 = Math.floor(Math.random() * 25 + 1);
+    let num2 = Math.floor(Math.random() * 25 + 1);
 }
 
-function checkAnswer () {
+function checkAnswer() {
 
 }
 
